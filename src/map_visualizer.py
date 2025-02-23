3ddc774c2dc942db27ecaec39411ae20271ca31f
Abdeali Poonawala <abdealip@umich.edu>
2025-02-23 12:15:48 -0500

    did some experiments with different sample periods

--- src/map_visualizer.py
+++ src/map_visualizer.py
@@ -7,22 +7,31 @@ config_file = os.path.join(base_dir, "config", "map_config.json")
 map_file = os.path.join(base_dir, "data", "test.map")
 
 grid = Grid3D.init_from_file(map_file)
-# occupied_points = grid.get_all_points_matching_value(1)
-
+occupied_points = grid.get_all_points_matching_value(1)
 not_occupied_points = grid.get_all_points_matching_value(0)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
+legend = []
 # ax.scatter(occupied_points[:, 0], occupied_points[:, 1], occupied_points[:, 2])
-ax.scatter(not_occupied_points[:, 0], not_occupied_points[:, 1], not_occupied_points[:, 2])
+# legend.append("Occupied")
 
-ax.axes.set_xlim3d(-0.5, 2.5)
-ax.axes.set_ylim3d(-0.5, 2.5)
-ax.axes.set_zlim3d(-0.5, 2.5)
+ax.scatter(not_occupied_points[:, 0], not_occupied_points[:, 1], not_occupied_points[:, 2])
+legend.append("Free")
+
+plt.legend(legend)
+plt.title("Free Space")
+
+ax.axes.set_xlim3d(-0.5, 1.0)
+ax.axes.set_ylim3d(-0.5, 1.0)
+ax.axes.set_zlim3d(-0.5, 1.0)
+ax.axes.set_box_aspect((1, 1, 1))
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.set_zlabel("z")
+
+ax.view_init(elev=30, azim=45, roll=0)
 
 plt.show()
--- src/mapper.py
+++ src/mapper.py
@@ -1,7 +1,10 @@
 import os
+import json
+import time
 from typing import List
 import numpy as np
-import json
+import pandas as pd
+
 from grid3D import Grid3D
 from forward_kinematics import Twist, TransformableCylinder
 
@@ -18,12 +21,12 @@ class Mapper:
         twists = self.config["joint_twists"]
         self.twists = [Twist(twist) for twist in twists]
 
-        links = self.config["links"]
+        links = self.config["mapping_links"]
         self.cylinders: List[TransformableCylinder] = []
         for link in links:
             if link["type"] == "cylinder":
                 n_twists = link["twists"]
-                self.cylinders.append(TransformableCylinder(link["mapping_radius"],
+                self.cylinders.append(TransformableCylinder(link["radius"],
                                                             link["length"],
                                                             link["axis"],
                                                             link["reference_config"],
@@ -43,10 +46,32 @@ class Mapper:
             cylinder.transform(joint_angles)
             self.map.set_all_points_within_cylinder_to_value(0, cylinder)
 
+    def consume_joint_log(self, joint_filename, sample_time_seconds):
+        df = pd.read_csv(joint_filename)
+        last_timestamp = 0
+        sample_time_nanoseconds = sample_time_seconds * 1e9
+        poses_consumed = 0
+        n_rows = len(df)
+        for i in range(n_rows):
+            row = df.iloc[i]
+            timestamp = row["timestamp"]
+            if timestamp - last_timestamp > sample_time_nanoseconds:
+                joint_angles = [row["joint1"], row["joint2"], row["joint3"], row["joint4"], row["joint5"], row["joint6"]]
+                self.consume_joint_angles(joint_angles)
+                poses_consumed += 1
+                if poses_consumed % 20 == 0:
+                    print(f"Consumed {poses_consumed} poses")
+                    print(f"Read {i+1} rows out of {n_rows}. {i/n_rows * 100:.2f}% complete")
+                last_timestamp = timestamp
+        print(f"Consumed {poses_consumed} poses")
+        print(f"Read {i+1} rows out of {n_rows}. {(i/n_rows * 100):.2f}% complete")
+
 if __name__ == "__main__":
     base_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)))
     config_file = os.path.join(base_dir, "config", "map_config.json")
     map_file = os.path.join(base_dir, "data", "test.map")
+    joint_log = os.path.join(base_dir, "data", "joint_states.csv")
     m = Mapper(config_file)
-    m.consume_joint_angles([np.pi/4, np.pi/6, -np.pi/2, 0, 0, 0])
+    m.consume_joint_log(joint_log, 0.1)
+    # m.consume_joint_angles([np.pi/4, np.pi/6, np.pi/2, 0, 0, 0])
     m.write_to_file(map_file)
